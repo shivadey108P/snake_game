a10bb6c5bede2c6bf55bcea00bd22f5f1ead6f89
shivadey108P <shivadey108@gmail.com>
2024-07-22 08:08:17 +0530

    Your commit message

--- day-20_snake_game/main.py
+++ day-20_snake_game/main.py
@@ -1,25 +1,29 @@
-from turtle import Turtle, Screen
+from turtle import Screen
+import time
+from snake import Snake
+from message_box import Pop_up
 
 screen = Screen()
+pop = Pop_up()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title('Snaky Sneaky')
+pop.open_pop_up('Welcome to snake game', 'Use arrow keys to move the snake')
+screen.tracer(0)
 
-segments = []
-
-for i in range(3):
-    new_segment = Turtle('square')
-    new_segment.color('white')
-    new_segment.penup()
-    new_segment.goto(-20 * i, 0)
-    segments.append(new_segment)
+snake = Snake()
 
 game_on =  True
 
-while
+screen.listen()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
-
-
-
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
 
 screen.exitonclick()
